5fd89fff2b502d4832aefdf3c5e915cfa127ab05
Blaker0000 <blaker00@icloud.com>
2025-04-08 14:27:46 -0600

    Remove reportWebVitals import and call

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-;
+import SOPDashboard from "./pages/SOPDashboard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SOPDashboard />
   </React.StrictMode>
 );
-
-
