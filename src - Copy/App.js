eed0e90e83908c54b8c112e7f47430d8029da153
Blaker0000 <blaker00@icloud.com>
2025-04-08 12:54:20 -0600

    Replace src with verified version for deploy

--- src - Copy/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SOPDashboard from "./pages/SOPDashboard";
-
-function App() {
-  return <SOPDashboard />;
-}
-
-export default App;
---
+++ src/src_verified_full/pages/SOPDashboard.js
@@ -0,0 +1,2 @@
+import React from 'react';
+export default function SOPDashboard() { return <div>SOP Dashboard Ready</div>; }
