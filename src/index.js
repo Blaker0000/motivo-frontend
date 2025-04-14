1dc35e8a98d26dee13196a37fb8e2cbbfa2e38dc
Blaker0000 <blaker00@icloud.com>
2025-04-13 23:30:34 -0600

    Fix: relative import path for SOPDashboard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import SOPDashboard from ".C:/Users/blake/motivo_frontend/src/pages/SOPDashboard.js";
+import SOPDashboard from "./pages/SOPDashboard.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
